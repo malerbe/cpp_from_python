0b3494400d71f6882c9e0b9cbbc03578a4a5e66f
lmalerba <louca0705@gmail.com>
2025-09-20 11:37:17 +0000

    End of session 2

--- cheatsheet.cpp
+++ cheatsheet.cpp
@@ -36,6 +36,89 @@ int a = 5;
 c = a++; // Post-increment: assign a to c, then a + 1
 // In both cases, a is incremented (a becomes 6), but c = 6 in the first case and c = 5 in the second
 
+// Loops:
+
+for (INIT; TEST; INCREMENT) 
+    // Instruction to repeat while TEST is true
+
+
+// example:
+for (int i=0; i<10; ++i) // Note that ++i is preferred to i++ for performance reasons (faster) 
+    std::cout << i << std::endl; // prints numbers from 0 to 9
+
+// If several instructions must be repeated, use braces { } to form a block:
+for (int i=0; i<10; ++i) {
+    std::cout << i << std::endl; // prints numbers from 0 to 9
+}
+
+// Arrays (tableaux):
+#define SIZE 10 // Preprocessor directive to define a constant
+
+// Tells the complier to replace all occurrences of SIZE with 10 before compilation
+
+int array[SIZE]; // Declare an array of 10 integers (uninitialized)
+
+for(int i = 0; i < SIZE; ++i) {
+scope("for");
+num tmp {"tmp", 2*i};
+tab[i] = tmp; // The 10 variables are tab[0], tab[1], ... tab[9]
+}
+
+// Functions
+int power(int a, int b); // Function declaration (prototype)
+// Declares a function named power that takes two integers and returns an integer and thus
+// the compiler will immediately allocate memory for both the return value and the two arguments
+// In C++, it's very common to declare functions before their first use and even the implementation of their content
+
+// We can then assign a value to a variable like this:
+c = power(a, b); // Calls the function power with arguments a and b, assigns the result to c by using a temporary value
+int c = power(a, b); // Calls the function power with arguments a and b, assigns the result to c directly
+
+// Example to illustrate functions:
+int power(int x, int n) {
+  fun_scope;
+
+  rem("You may have noticed that arguments have just been");
+  rem("copied to two new variables on the stack.");
+
+  // We can use symbols x and n to name those two copies.
+
+  ___;
+
+  // Let us use another variable for the result.
+  int res     {"res",     1};
+  rem("Indeed, res is not really allocated here... read comments in the code.");
+  ___;
+
+  for(int counter {"counter", 1}; counter <= n; ++counter)
+    res *= x;
+
+  ___;
+
+  // "return" exits the function, but it also tells the compiler that
+  // the "res" variable do not need to be allocated, since the memory
+  // room needed for it is already on the stack, at the "slot" where
+  // the result of the function is expected to be.
+  return res;
+}
+
+
+// Functions by reference
+void double_raz(num& x, num y) {
+  fun_scope;
+  
+  x = 0;
+  y = 0;
+}
+
+
+/*
+In this case, if we put a reference to an 'x' variable when calling the function,
+the function will modify the original variable, not a copy of it.
+Outside of the scope of the function, the variable will be modified to be 0. 
+
+= "Effet de bord" (side effect)
+*/
 
 
 // Scopes
